72edec36e6e76a05f0bc3e27e1637740734d5ba4
Mike Galbraith <efault@gmx.de>
2017-09-03 04:48:10 +0200

    kernel/hrtimer/hotplug: don't wake ktimersoftd while holding the hrtimer base lock
    kernel/hrtimer: don't wakeup a process while holding the hrtimer base lock
    missed a path, namely hrtimers_dead_cpu() -> migrate_hrtimer_list().  Defer
    raising softirq until after base lock has been released there as well.
    
    Signed-off-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1823,7 +1823,7 @@ int hrtimers_prepare_cpu(unsigned int cpu)
 }
 
 #ifdef CONFIG_HOTPLUG_CPU
-static void migrate_hrtimer_list(struct hrtimer_clock_base *old_base,
+static int migrate_hrtimer_list(struct hrtimer_clock_base *old_base,
 				 struct hrtimer_clock_base *new_base,
 				 bool remove_pinned)
 {
@@ -1876,16 +1876,20 @@ static void migrate_hrtimer_list(struct hrtimer_clock_base *old_base,
 
 #ifdef CONFIG_PREEMPT_RT_BASE
 	list_splice_tail(&old_base->expired, &new_base->expired);
-	if (!list_empty(&new_base->expired))
-		raise_softirq_irqoff(HRTIMER_SOFTIRQ);
+	/*
+	 * Tell the caller to raise HRTIMER_SOFTIRQ.  We can't safely
+	 * acquire ktimersoftd->pi_lock while the base lock is held.
+	 */
+	return !list_empty(&new_base->expired);
 #endif
+	return 0;
 }
 
 static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 {
 	struct hrtimer_cpu_base *old_base, *new_base;
 	unsigned long flags;
-	int i;
+	int i, raise = 0;
 
 	local_irq_save(flags);
 	old_base = &per_cpu(hrtimer_bases, scpu);
@@ -1898,12 +1902,15 @@ static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 	raw_spin_lock_nested(&old_base->lock, SINGLE_DEPTH_NESTING);
 
 	for (i = 0; i < HRTIMER_MAX_CLOCK_BASES; i++) {
-		migrate_hrtimer_list(&old_base->clock_base[i],
-				     &new_base->clock_base[i], remove_pinned);
+		raise |= migrate_hrtimer_list(&old_base->clock_base[i],
+					      &new_base->clock_base[i], remove_pinned);
 	}
 
 	raw_spin_unlock(&old_base->lock);
 	raw_spin_unlock(&new_base->lock);
+
+	if (raise)
+		raise_softirq_irqoff(HRTIMER_SOFTIRQ);
 
 	/* Check, if we got expired work to do */
 	__hrtimer_peek_ahead_timers();
