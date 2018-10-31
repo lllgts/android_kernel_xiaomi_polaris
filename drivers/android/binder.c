70a51bd28e3987a3baca8c7519033a8350e9061b
Jignesh Patel <jignesh@motorola.com>
2018-10-31 15:18:12 +0530

    binder: Fix concurrency issue in target_thread wakeup
    until wake_up_interruptible_sync returns, couldn't another thread
    just take control of the CPU anyway before the return.
    This call will also prevent a reschedule.
    
    Should be use wake_up_interruptible() call to immediately reschedule
    target_thread instead of using wake_up_interruptible_sync() to block
    the target_thread until CPU available to run.
    
    Also protect target_thread with spinlock.
    
    Change-Id: Ib2702bc798bf6927cee753811b39032f0aefe166
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1264379
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3553,6 +3553,7 @@ static void binder_transaction(struct binder_proc *proc,
 		binder_pop_transaction_ilocked(target_thread, in_reply_to);
 		binder_enqueue_thread_work_ilocked(target_thread, &t->work);
 		target_proc->outstanding_txns++;
+		wake_up_interruptible(&target_thread->wait);
 		binder_inner_proc_unlock(target_proc);
 		if (in_reply_to->is_nested) {
 			spin_lock(&thread->prio_lock);
@@ -3560,7 +3561,6 @@ static void binder_transaction(struct binder_proc *proc,
 			thread->prio_next = in_reply_to->saved_priority;
 			spin_unlock(&thread->prio_lock);
 		}
-		wake_up_interruptible_sync(&target_thread->wait);
 		binder_restore_priority(thread, &in_reply_to->saved_priority);
 		binder_free_transaction(in_reply_to);
 	} else if (!(t->flags & TF_ONE_WAY)) {
