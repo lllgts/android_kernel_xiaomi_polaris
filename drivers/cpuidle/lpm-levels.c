6fb7b80fbf4f0f274557acc8635370ed60caaae0
Amir Vajid <avajid@codeaurora.org>
2019-09-10 17:38:44 -0700

    cpuidle: lpm-levels: get bias time from scheduler
    Update lpm_disallowed() to receive bias_time directly
    from the scheduler.
    
    Change-Id: I756c4f5e545083b455041af5ad8ddf7ee7986365
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -55,7 +55,6 @@
 #define SCLK_HZ (32768)
 #define PSCI_POWER_STATE(reset) (reset << 30)
 #define PSCI_AFFINITY_LEVEL(lvl) ((lvl & 0x3) << 24)
-#define BIAS_HYST (bias_hyst * NSEC_PER_MSEC)
 
 static struct system_pm_ops *sys_pm_ops;
 static DEFINE_SPINLOCK(bc_timer_lock);
@@ -544,29 +543,22 @@ static void clear_predict_history(void)
 
 static void update_history(struct cpuidle_device *dev, int idx);
 
-static inline bool is_cpu_biased(int cpu)
-{
-	u64 now = sched_clock();
-	u64 last = sched_get_cpu_last_busy_time(cpu);
-
-	if (!last)
-		return false;
-
-	return (now - last) < BIAS_HYST;
-}
-
 static inline bool lpm_disallowed(s64 sleep_us, int cpu, struct lpm_cpu *pm_cpu)
 {
 	uint64_t bias_time = 0;
 
-	if (sleep_disabled && !cpu_isolated(cpu))
+	if (cpu_isolated(cpu))
+		goto out;
+
+	if (sleep_disabled)
 		return true;
 
-	if (is_cpu_biased(cpu, &bias_time) && (!cpu_isolated(cpu))) {
-		pm_cpu->bias = bias_time;
+	bias_time = sched_lpm_disallowed_time(cpu);
+	if (bias_time) {
 		return true;
 	}
 
+out:
 	if (sleep_us < 0)
 		return true;
 
