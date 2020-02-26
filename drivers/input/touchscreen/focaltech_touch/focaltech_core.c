5688873be82fc03e231a7d59f2a9ef324dd60372
palaych <palaychm@yandex.ru>
2020-02-26 23:48:41 +0300

    drivers: touchscreen: remove unused memory allocation and return hwinfo
    Signed-off-by: palaych <palaychm@yandex.ru>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -2235,6 +2235,8 @@ static int fts_ts_probe(struct i2c_client *client, const struct i2c_device_id *i
 #endif
 	xiaomitouch_register_modedata(&xiaomi_touch_interfaces);
 #endif
+	update_hardware_info(TYPE_TOUCH, 3);
+	update_hardware_info(TYPE_TP_MAKER, ts_data->lockdown_info[0] - 0x30);
 
 	FTS_FUNC_EXIT();
 	return 0;
