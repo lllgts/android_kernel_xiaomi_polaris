99981f99a92aa811240caf89d8288713088d355e
Huaixin Chang <changhuaixin@linux.alibaba.com>
2020-03-11 11:07:54 +0800

    alinux: sched: Add cfs bandwidth burst statistics
    Introduce statistics exports for the burstable cfs bandwidth
    controller.
    
    The following exports are included:
    
    current_bw: current runtime in global pool
    nr_burst:   number of periods bandwidth burst occurs
    burst_time: cumulative wall-time that any cpus has
    	    used above quota in respective periods
    
    Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>
    Signed-off-by: Huaixin Chang <changhuaixin@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9407,7 +9407,10 @@ static int tg_set_cfs_bandwidth(struct task_group *tg, u64 period, u64 quota,
 				cfs_b->runtime = RUNTIME_INF;
 		}
 	}
-	/* restart the period timer (if active) to handle new period expiry */
+
+	cfs_b->previous_runtime = cfs_b->runtime;
+
+	/* Restart the period timer (if active) to handle new period expiry: */
 	if (runtime_enabled)
 		start_cfs_bandwidth(cfs_b, 1);
 	raw_spin_unlock_irq(&cfs_b->lock);
@@ -9637,6 +9640,20 @@ static int cpu_stats_show(struct seq_file *sf, void *v)
 	seq_printf(sf, "nr_periods %d\n", cfs_b->nr_periods);
 	seq_printf(sf, "nr_throttled %d\n", cfs_b->nr_throttled);
 	seq_printf(sf, "throttled_time %llu\n", cfs_b->throttled_time);
+
+	if (schedstat_enabled() && tg != &root_task_group) {
+		u64 ws = 0;
+		int i;
+
+		for_each_possible_cpu(i)
+			ws += schedstat_val(tg->se[i]->statistics.wait_sum);
+
+		seq_printf(sf, "wait_sum %llu\n", ws);
+	}
+
+	seq_printf(sf, "current_bw %llu\n", cfs_b->runtime);
+	seq_printf(sf, "nr_burst %d\n", cfs_b->nr_burst);
+	seq_printf(sf, "burst_time %llu\n", cfs_b->burst_time);
 
 	return 0;
 }
