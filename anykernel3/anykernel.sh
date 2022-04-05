611d1900975cd687b1667d88f9e77f69f7695613
lllgts <wuer031@126.com>
2022-04-05 02:47:52 +0000

    Push anykernel

---
+++ anykernel3/anykernel.sh
@@ -0,0 +1,30 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Sakura_kernel by@lllgts
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=polaris
+supported.versions=9 - 12
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+write_boot;
+## end install
