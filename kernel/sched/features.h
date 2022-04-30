36a91ad2e37d8766ebbdbdba7c1713fdf840415a
Kyuofox <88403766@qq.com>
2022-04-30 11:47:16 +0800

    sched: Enable TTWU_QUEUE when HMP is disabled
    TTWU_QUEUE is disabled when HMP is introduced.
    
    We found that TTWU_QUEUE is enabled when using EAS on msm-4.14 or
    msm-4.19.
    
    So,set a limit and enable it.
    
    Signed-off-by: Kyuofox <88403766@qq.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -55,7 +55,11 @@ SCHED_FEAT(NONTASK_CAPACITY, true)
  * Queue remote wakeups on the target CPU and process them
  * using the scheduler IPI. Reduces rq->lock contention/bounces.
  */
+#ifdef CONFIG_SCHED_HMP
 SCHED_FEAT(TTWU_QUEUE, false)
+#else
+SCHED_FEAT(TTWU_QUEUE, true)
+#endif
 
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
