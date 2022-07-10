6cd1dea3bfc05cc40e1e42859e29a8c68b769b61
lllgts <wuer031@126.com>
2022-07-10 08:52:12 +0000

    Revert "cpufreq: schedutil: Fix for limits update with fast switch enabled"
    This reverts commit 16208b8818a2851b0e6e9b941fcb82e9a0834d63.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1068,8 +1068,6 @@ static void sugov_limits(struct cpufreq_policy *policy)
 {
 	struct sugov_policy *sg_policy = policy->governor_data;
 	unsigned long flags;
-	unsigned int ret;
-	int cpu;
 
 	if (!policy->fast_switch_enabled) {
 		mutex_lock(&sg_policy->work_lock);
@@ -1080,12 +1078,7 @@ static void sugov_limits(struct cpufreq_policy *policy)
 		cpufreq_policy_apply_limits(policy);
 		mutex_unlock(&sg_policy->work_lock);
 	} else {
-		ret = cpufreq_policy_apply_limits_fast(policy);
-		if (ret && policy->cur != ret) {
-			policy->cur = ret;
-			for_each_cpu(cpu, policy->cpus)
-				trace_cpu_frequency(ret, cpu);
-		}
+		cpufreq_policy_apply_limits_fast(policy);
 	}
 
 	sg_policy->need_freq_update = true;
