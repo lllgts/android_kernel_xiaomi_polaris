18a473e8d53d01dd1330b8565d5ab738e83c8fdc
lllgts <wuer031@126.com>
2022-12-25 09:54:27 +0000

    rcu: Fix build errors after inline 5.4 rcu
    Signed-off-by: lllgts <wuer031@126.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -52,8 +52,6 @@
 #include <linux/oom.h>
 #include <linux/smpboot.h>
 #include <linux/jiffies.h>
-#include <linux/sched/isolation.h>
-#include <linux/sched/clock.h>
 #include "../time/tick-internal.h"
 
 #include "tree.h"
