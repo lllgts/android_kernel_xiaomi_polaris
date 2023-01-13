14cc9ed0df1051269f437085a379971b6b2b58dd
lllgts <wuer031@126.com>
2023-01-13 13:03:18 +0000

    kernel/sysctl: Fix compilation after merge c5ce586
    Signed-off-by: lllgts <wuer031@126.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -152,7 +152,6 @@ const int sched_user_hint_max = 1000;
 static unsigned int ns_per_sec = NSEC_PER_SEC;
 static unsigned int one_hundred_thousand = 100000;
 #endif
-static int two_hundred_fifty_five = 255;
 /* this is needed for the proc_doulongvec_minmax of vm_dirty_bytes */
 static unsigned long dirty_bytes_min = 2 * PAGE_SIZE;
 
@@ -743,7 +742,7 @@ static struct ctl_table kern_table[] = {
 		.data		= &sched_lib_mask_force,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
-		.proc_handler	= proc_douintvec_minmax,
+		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 		.extra2		= &two_hundred_fifty_five,
 	},
